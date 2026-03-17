9a0636b67f2028d05dcc5ffaaeaae0d7300ecd75
stefaniebarfoot <stefanie.barfoot@gmail.com>
2026-03-17 12:17:15 +0000

    added icon data

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ function updateWeatherData(response) {
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
 
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"
+                class="weather-app-icon"
+              />`;
+
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
