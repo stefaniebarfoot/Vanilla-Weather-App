a0d470c79e37ce3545493cc7b1035f9d7f749518
stefaniebarfoot <stefanie.barfoot@gmail.com>
2026-03-17 13:43:26 +0000

    Added advanced JS to forecast

--- src/index.js
+++ src/index.js
@@ -51,7 +51,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">☀️</div>
+            <div class="weather-forecast-units">
+              <div class="weather-forecast-temperatures">
+                <strong>15°</strong>
+              </div>
+              <div class="weather-forecast-temperatures">9°</div>
+            </div>
+          </div>`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("London");
+displayForecast();
